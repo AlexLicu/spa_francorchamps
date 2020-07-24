667e55cce5cf15fa779c847f4cb612989e87170b
Alex Licu <57290562+AlexLicu@users.noreply.github.com>
2020-07-24 15:09:48 +0300

    Add files via upload

---
+++ spa/fxmanifest.lua
@@ -0,0 +1,15 @@
+
+description "Spa-Francorchamps map mod converted for FiveM"
+author 'GTA Belgium,Tanjes, onclsam13, Iceman/Licu'
+version '1.0.0'
+
+fx_version 'bodacious'
+games { 'gta5' }
+
+this_is_a_map 'yes'
+files {
+	'gtxd.meta',
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/data/spa.ytyp'
+data_file 'GTXD_PARENTING_DATA' 'gtxd.meta'
